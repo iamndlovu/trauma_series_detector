fc233665bf36c134c1f305c613a25ce8b72853dd
iamndlovu <pndlovu385@gmail.com>
2022-03-23 23:20:57 +0200

    client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { ImageUpload } from "./home";
+
+function App() {
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      width: "100%",
+      height: "100%"
+    }}>
+      <ImageUpload />
+    </div>
+  );
+}
+
+export default App;
