3cdee006f730f468bdee44f306833af1f68d75ed
iamndlovu <pndlovu385@gmail.com>
2022-04-11 04:54:24 +0200

    Form to capture patient information

---
+++ client/src/components/form/PatientDetailsForm.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import styles from './PatientDetailsForm.module.css';
+
+const PatientDetailsForm = ({ formData, formControls }) => {
+	const {
+		patientFirstName,
+		patientLastName,
+		hospitalNumber,
+		patientID,
+		referringClinician,
+	} = formData;
+
+	const {
+		onChangeFirstName,
+		onChangeLastName,
+		onChangeHospitalNumber,
+		onChangeClinician,
+		onChangePatientID,
+		submitForm,
+	} = formControls;
+
+	return (
+		<form className={styles.Form} onSubmit={submitForm}>
+			<div className={styles.formGroup}>
+				<label className={styles.offscreen}>Hospital Number</label>
+				<input
+					type="text"
+					required
+					placeholder="Hospital Number"
+					value={hospitalNumber}
+					onChange={onChangeHospitalNumber}
+				/>
+			</div>
+			<div className={styles.formGroup}>
+				<label className={styles.offscreen}>Patient First Name</label>
+				<input
+					type="text"
+					required
+					placeholder="Patient First Name"
+					value={patientFirstName}
+					onChange={onChangeFirstName}
+				/>
+			</div>
+			<div className={styles.formGroup}>
+				<label className={styles.offscreen}>Patient Last Name(s)</label>
+				<input
+					type="text"
+					required
+					placeholder="Patient Last Name(s)"
+					value={patientLastName}
+					onChange={onChangeLastName}
+				/>
+			</div>
+			<div className={styles.formGroup}>
+				<label className={styles.offscreen}>Patient ID</label>
+				<input
+					type="text"
+					required
+					placeholder="Patient ID"
+					value={patientID}
+					onChange={onChangePatientID}
+				/>
+			</div>
+			<div className={styles.formGroup}>
+				<label className={styles.offscreen}>Referring Clinician</label>
+				<input
+					type="text"
+					required
+					placeholder="Referring Clinician"
+					value={referringClinician}
+					onChange={onChangeClinician}
+				/>
+			</div>
+			<input type="submit" value="Enter" className={styles.submit} />
+		</form>
+	);
+};
+
+export default PatientDetailsForm;
